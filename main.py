aeb7e0d876af5b823196295d6afb3c0dec6cee9a
Frederik Palmø <frederik@palmoe.dk>
2019-11-11 22:06:10 +0100

    oprettede filen

---
+++ main.py
@@ -0,0 +1,23 @@
+x = 1
+
+print("(1) Start")
+print("(2) Options")
+print("(3) Exit")
+
+choice = input("Vælg: ")
+print(choice)
+
+if choice == 1:
+    print("Du valgte start")
+elif choice == 2:
+    print("Du valgte options")
+elif choice == 3:
+    print("Du valgte exit")
+else:
+    print("Du valgte forkert eller noget")
+
+
+
+
+# Mål; lave et power creep rpg
+
